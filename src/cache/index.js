9e5969da97f445c2402fe226f57557ee06f6b758
Muhammad Afnan <muhammadafnan@Muhammads-MacBook-Pro.local>
2023-09-17 15:27:43 +0500

    containerization

--- src/cache/index.js
+++ src/cache/index.js
@@ -1,6 +1,7 @@
 const { reject } = require('async');
 const Redis = require('redis')
 const client = Redis.createClient({
+    url: 'redis://redis:6379',
   legacyMode: true
 });
 (async () => { await client.connect(); })();
